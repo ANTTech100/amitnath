f5553567679a2faf0dd17e800390ef51a85b27d6
saurabh4323 <saurabhiitr01@gmail.com>
2025-07-13 00:15:47 +0530

    rr

--- app/layouts/layoutsix/[id]/page.js
+++ app/layouts/layoutsix/[id]/page.js
@@ -2,7 +2,8 @@
 import { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 import Head from "next/head";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
+import { motion } from "framer-motion/dist/framer-motion";
 
 // Enhanced animation variants
 const fadeInVariants = {
